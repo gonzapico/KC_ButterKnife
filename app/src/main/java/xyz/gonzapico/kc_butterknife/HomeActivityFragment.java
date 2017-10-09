d2c8f8f09466077a776ab7e74f364c47d36ef761
Gonzalo Fernández <gonzalo.fernandez@24imedia.com>
2017-10-09 22:53:33 +0200

    ButterKnife - Fragment

--- app/src/main/java/xyz/gonzapico/kc_butterknife/HomeActivityFragment.java
+++ app/src/main/java/xyz/gonzapico/kc_butterknife/HomeActivityFragment.java
@@ -2,19 +2,39 @@ package xyz.gonzapico.kc_butterknife;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnTextChanged;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class HomeActivityFragment extends Fragment {
 
+  private final static String TAG = "Fragment";
+
+  @BindView(R.id.tvWelcome) TextView tvWelcome;
+  @OnTextChanged(value = R.id.etWelcome,
+      callback = OnTextChanged.Callback.AFTER_TEXT_CHANGED)
+  void afterEmailInput(Editable editable) {
+    tvWelcome.setText(editable.toString());
+  }
+
+  @BindString(R.string.welcome_message) String welcomeMsg;
+
   public HomeActivityFragment() {
   }
 
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_home, container, false);
+    View view = inflater.inflate(R.layout.fragment_home, container, false);
+    ButterKnife.bind(this, view);
+    tvWelcome.setText(welcomeMsg);
+    return view;
   }
 }
